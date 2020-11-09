bb04bdf88dae07dd9b4349b3d63f70a00ad79de0
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2020-11-09 15:53:53 -0300

    Implementada a funcionalidade principal

---
+++ README.md
@@ -0,0 +1,34 @@
+<p align="center">
+  <img src="https://estudiodigitalbocca.com.br/edb-logo.svg" width="200px">
+  <p align="center">Ultima Atualização na v0.2.0</p>
+  <h1 align="center">create-edb-app</h1>
+  <h4 align="center">
+    Iniciador de Projetos do Estúdio Digital Bocca
+  </h4>
+  <p align="center">
+    <img src="https://badgen.net/badge/version/v0.2.0/orange">
+  </p>
+  <p align="center">(c)2020 - Estúdio Digital Bocca</p>
+</p>
+
+---
+
+# create-edb-app - README
+
+---
+
+**Este produto está em desenvolvimento. Não use em produção**
+
+## UTILIZAÇÃO
+
+```bash
+npm init edb-app <starter> [pasta]
+```
+
+Lista dos starters:
+
+- spa-start - SPA com Vue, PUG e Parcel - `npm init edb-app spa-start nova-spa`
+- electron-start - Electron com Vue e SASS - `npm init edb-app electron-start novo-electron`
+- api-boilerplate - Rest API com Express - `npm init edb-app api-boilerplate nova-api`
+
+> (c)2020 - Estúdio Digital Bocca
---
+++ src/index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const { program } = require('commander')
+const degit = require('degit')
+const chalk = require('chalk')
+const path = require('path')
+
+program.version('0.2.0')
+
+program.arguments('<starter> [pasta]')
+
+program.description('Executa o programa', {
+  starter: 'O pacote que será copiado.',
+  pasta: 'A pasta que será criada com os arquivos.'
+})
+
+program.action(async (starter, pasta) => {
+  try {
+    console.log(chalk.yellow('Cortesia do Estúdio Digital Bocca'))
+    console.log(chalk.green('Localizando Arquivos...'))
+    const emmiter = degit(`digitalbocca/edb-${starter}`, {
+      cache: false,
+      force: true,
+      verbose: true
+    })
+    emmiter.on('info', info => console.log(info.message))
+    await emmiter.clone(path.resolve((pasta || '.')))
+
+    console.log(chalk.green('Seu novo Projeto está pronto.'))
+    console.log(chalk.yellow('Lembre-se de instalar as dependências.'))
+  } catch (e) {
+    console.error(e.message)
+  }
+})
+
+program.parse(process.argv)
