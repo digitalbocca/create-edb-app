0d8b965f16c0f81b274ddf7b0fb47472ff623c2b
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2023-08-11 21:29:13 -0300

    Adiciona vue-vite

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 <p align="center">
   <img src="https://estudiodigitalbocca.com.br/edb-logo.svg" width="200px">
-  <p align="center">Ultima Atualização na v0.8.0</p>
   <h1 align="center">create-edb-app</h1>
   <h4 align="center">
     Iniciador de Projetos do Estúdio Digital Bocca
   </h4>
   <p align="center">
-    <img src="https://badgen.net/badge/version/v0.8.0/orange">
+    <img src="https://badgen.net/badge/version/v0.9.0/orange">
   </p>
-  <p align="center">(c)2020-2021 Estúdio Digital Bocca</p>
+  <p align="center">(c)2020-2023 Estúdio Digital Bocca</p>
 </p>
 
 ---
@@ -27,9 +26,10 @@ npm init @digitalbocca/edb-app <starter> [pasta]
 
 Lista dos starters:
 
-- electron-vue3 - Electron com Vue 3, Vuex 4, Vue Router e SASS - `npm init @digitalbocca/edb-app electron-vue3 meu-projeto-electron`
+- vue-vite - Vue 3, Pinia, Vue Router, Vite e várias ferramentas pré configuradas - `npm init @digitalbocca/edb-app vue-vite meu-projeto-vue`
+- electron-vue3 - Electron com Vue 3, Pinia, Vue Router e Vite - `npm init @digitalbocca/edb-app electron-vue3 meu-projeto-electron`
 - spa-start - SPA com Vue 3+, Vuex 4+, Vue Router 4+, PUG e Parcel 2+ - `npm init @digitalbocca/edb-app spa-start nova-spa`
 - electron-start - Electron com Vue e SASS - `npm init @digitalbocca/edb-app electron-start novo-electron`
 - api-express - Rest API com Express - `npm init @digitalbocca/edb-app api-express nova-api`
 
-> (c)2020-2021 Estúdio Digital Bocca
+> (c)2020-2023 Estúdio Digital Bocca
--- src/index.js
+++ src/index.js
@@ -2,37 +2,33 @@
 
 const { program } = require('commander')
 const degit = require('degit')
-const chalk = require('chalk')
+const pc = require('picocolors')
 const path = require('path')
 
-program.version('0.8.0')
+program.version('0.9.0')
 
-program.arguments('<starter> [pasta]')
-
-program.description('Executa o programa', {
-  starter: 'O pacote que será copiado.',
-  pasta: 'A pasta que será criada com os arquivos.'
-})
+program.argument('<starter>', 'O pacote que será copiado.')
+program.argument('[pasta]', 'A pasta que será criada com os arquivos.')
 
 program.action(async (starter, pasta) => {
   try {
-    console.log(chalk.yellow('Cortesia do Estúdio Digital Bocca'))
-    console.log(chalk.green('Localizando Arquivos...'))
+    console.log(pc.yellow('Cortesia do Estúdio Digital Bocca'))
+    console.log(pc.green('Localizando Arquivos...'))
 
     const starterName = () => {
-      return starter === 'electron-vue3' ? `digitalbocca/electron-vue3#main` : `digitalbocca/edb-${starter}#main`
+      return starter === 'electron-vue3' ? 'digitalbocca/electron-vue3#main' : `digitalbocca/edb-${starter}#main`
     }
 
-    const emmiter = degit(starterName(), {
+    const emitter = degit(starterName(), {
       cache: false,
       force: true,
       verbose: true
     })
-    emmiter.on('info', info => console.log(info.message))
-    await emmiter.clone(path.resolve((pasta || '.')))
+    emitter.on('info', info => console.log(info.message))
+    await emitter.clone(path.resolve((pasta || '.')))
 
-    console.log(chalk.green('Seu novo Projeto está pronto.'))
-    console.log(chalk.yellow('Lembre-se de instalar as dependências.'))
+    console.log(pc.green('Seu novo Projeto está pronto.'))
+    console.log(pc.yellow('Lembre-se de instalar as dependências.'))
   } catch (e) {
     console.error(e.message)
   }
